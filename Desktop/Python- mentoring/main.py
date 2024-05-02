f8998231a66f17015cc280c80b98c5d0c5b64d98
Gabriela <gabi007gaj@vp.pl>
2024-05-02 11:49:52 +0200

    Molecules-version-2

--- Desktop/Python- mentoring/main.py
+++ Desktop/Python- mentoring/main.py
@@ -18,9 +18,10 @@ db: List[Molecule] = [
     Molecule(name="Water", formula="H20", weight="18u")
 ]
 
-file_path = "C:\\Users\\gabi0\\Desktop\\chemical.txt"
+file_path = "C:\\Users\\gabi0\\Desktop\\chemical.json"
 
-# Do not know how to save this in json format 
+
+# Do not know how to save this in json format
 
 
 @app.post("/molecules")
@@ -28,16 +29,13 @@ async def add_molecule(molecule: Molecule):
     db.append(molecule)
     if os.path.getsize(file_path) == 0:
         with open(file_path, "w+") as file:
+            # json.dumps(db, indent=2)
             file.write(f"{db}")
-            # json.dump(db, file)
     else:
         with open(file_path, "a") as file:
             file.write(f"{molecule}")
             # json.dump(molecule, file)
     return
-
-
-# Do not know how to test that function in Postman
 
 
 @app.get("/molecules")
@@ -50,6 +48,7 @@ async def fetch_molecules(name: str | None = None):
         return db
 
 
+# DB for testing
 '''
 db: List[Molecule] = [
     {"name":"Ethanol","formula":"C2H5OH","weight":"46u"}
